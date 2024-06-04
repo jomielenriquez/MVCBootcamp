9c9cd35b26e00af5ff62d86e0cfa191a37a857b7
Jomiel <enriquez.jliquigan@gmail.com>
2024-06-04 08:23:23 +0800

    test

--- SampleWeb/obj/Debug/net8.0/SampleWeb.AssemblyInfo.cs
+++ SampleWeb/obj/Debug/net8.0/SampleWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551450a9a167fdea878b0895cd55509066599e9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283022fbad7ea9b97f2015a2432cd5c9c90bec08")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
